d2aa58dfbbb8b11140df72c106cbc3951f35e6f3
P4UL0_O_DEV <pauloricardoodev@gmail.com>
2025-11-09 18:44:07 -0300

    Refactor and restore essential global variables

--- CONSULTE(C)_AEP.c
+++ CONSULTE(C)_AEP.c
@@ -43,13 +43,12 @@ void pause()
 {
 
     printf("\n	ENTER para continuar");
-    getchar();//conosme a entrada e limpa o buffer
+    getchar(); // conosme a entrada e limpa o buffer
 }
-
 
 void menu()
 {
-	printf("\n------------------------------------------------------------------------------------------------------------------------\n");
+    printf("\n------------------------------------------------------------------------------------------------------------------------\n");
     printf("\n\t						M E N U\n");
     printf("\n		[1] Coletar informações do Paciente\n");
     printf("\n		[2] Iniciar Triagem\n");
@@ -60,7 +59,7 @@ void menu()
     printf("\n------------------------------------------------------------------------------------------------------------------------\n");
     printf("Opção: ");
     scanf("%d", &opcao);
-    getchar();//limpa o buffer
+    getchar(); // limpa o buffer
 
     switch (opcao)
     {
@@ -94,24 +93,24 @@ void informacoesPaciente()
 
     char nomeTmp[100];
     int i;
-	printf("\n------------------------------------------------------------------------------------------------------------------------\n");	
-	printf("\n");
-	printf("					Perfeito vamos cadastrar o paciente!");
-	printf("\n");
-	printf("\n------------------------------------------------------------------------------------------------------------------------\n");
-	
+    printf("\n------------------------------------------------------------------------------------------------------------------------\n");
+    printf("\n");
+    printf("					Perfeito vamos cadastrar o paciente!");
+    printf("\n");
+    printf("\n------------------------------------------------------------------------------------------------------------------------\n");
+
     printf("\n 		INFORME O NOME: ");
     fgets(nomeTmp, sizeof(nomeTmp), stdin);
     nomeTmp[strcspn(nomeTmp, "\n")] = '\0';
 
-    for(i=0;i<strlen(nomeTmp);i++){
+    for (i = 0; i < strlen(nomeTmp); i++)
+    {
         nome[i] = toupper(nomeTmp[i]);
     }
 
-
     printf("\n 	       INFORME A IDADE: ");
     scanf("%d", &idade);
-    getchar();//limpa o buffer
+    getchar(); // limpa o buffer
 
     printf("\n            INFORME O TELEFONE: ");
     fgets(telefone, sizeof(telefone), stdin);
@@ -124,13 +123,13 @@ void informacoesPaciente()
 void iniciarTriagem()
 {
     system("cls");
-    printf("\n------------------------------------------------------------------------------------------------------------------------\n");	
-	printf("\n");
-	
-	printf("			      Triagem iniciada, digite (S) para sim e (N) para não\n");
-	printf("\n");
-	printf("\n------------------------------------------------------------------------------------------------------------------------\n");
-    //int pontos = 0;
+    printf("\n------------------------------------------------------------------------------------------------------------------------\n");
+    printf("\n");
+
+    printf("			      Triagem iniciada, digite (S) para sim e (N) para não\n");
+    printf("\n");
+    printf("\n------------------------------------------------------------------------------------------------------------------------\n");
+    // int pontos = 0;
     int febre = perguntasTriagem("	Febre: ");
     int tosse = perguntasTriagem("	Tosse: ");
     int dorG = perguntasTriagem("	Dor de garganta: ");
@@ -142,12 +141,16 @@ void iniciarTriagem()
     pontos += febre + tosse + dorG + ar + contato + dorCab + fraq;
 
     // Critérios especiais
-    if (ar) pontos += 40;
-    if (febre && tosse) pontos += 30;
-    if (contato) pontos += 20;
+    if (ar)
+        pontos += 40;
+    if (febre && tosse)
+        pontos += 30;
+    if (contato)
+        pontos += 20;
 
     // Idade influencia
-    if (idade <= 10 || idade >= 60) pontos += 15;
+    if (idade <= 10 || idade >= 60)
+        pontos += 15;
 
     // ----- ÁRVORE REFINADA -----
     if (pontos >= 90)
@@ -160,7 +163,7 @@ void iniciarTriagem()
     {
         strcpy(quadroPaciente, "	RISCO MODERADO-ALTO	");
         strcpy(recomendacoes, "	AVALIAÇÃO PRESENCIAL	");
-        strcpy(conclusao, "	GRAVE	");
+        strcpy(conclusao, " GRAVE	");
     }
     else if (pontos >= 45)
     {
@@ -182,8 +185,8 @@ void iniciarTriagem()
     }
 
     calculoUrgencia(pontos);
-	printf("\n");
-    printf("===============================================Tragem Finalizada========================================================\n\n");
+    printf("\n");
+    printf("===============================================Triagem Finalizada========================================================\n\n");
 }
 
 int perguntasTriagem(char *msg)
@@ -195,33 +198,33 @@ int perguntasTriagem(char *msg)
     {
         printf("%s (S/N): ", msg);
         scanf(" %c", &resposta);
-        getchar();//limpar buffer
+        getchar(); // limpar buffer
 
         if (toupper(resposta) == 'S')
         {
-           do {
+            do
+            {
                 printf("\n		(1=Leve / 2=Moderado / 3=Forte): ");
                 scanf("%d", &nivel);
-                getchar();// limpar buffer
+                getchar(); // limpar buffer
 
-                if(nivel < 1 || nivel > 3){
+                if (nivel < 1 || nivel > 3)
+                {
                     printf("\n ! intensidade inválida, tente novamente ! \n");
                 }
-            }
-            while (nivel < 1 || nivel > 3);
+            } while (nivel < 1 || nivel > 3);
 
             printf("\n\n");
             return nivel * 10;
         }
-        if (toupper(resposta) == 'N'){
+        if (toupper(resposta) == 'N')
+        {
             return 0;
         }
 
         printf("		Entrada inválida!\n");
     }
-
 }
-
 
 void exibirPrognostico()
 {
@@ -233,7 +236,7 @@ void exibirPrognostico()
     printf("\n");
     printf("\n						Paciente: %s\n", nome);
     printf("\n						Idade: %d\n", idade);
-	printf("\n");
+    printf("\n");
     printf("\n= 		          ==================================================\n");
     printf("\n");
     printf("						Quadro: %s\n", quadroPaciente);
@@ -248,41 +251,41 @@ void observacoes()
 {
     system("cls");
 
-    if (strcmp(conclusao, "CRITICO")==0)
+    if (strcmp(conclusao, "	!!CRITICO!!	") == 0)
     {
         strcpy(orientacoes,
-                 "Paciente em estado CRÍTICO. AÇÕES IMEDIATAS:\n"
-                 "- Encaminhar para emergência/UTI imediatamente; não administrar medicamentos sem prescrição;\n"
-                 "- Evitar esforços, manter via aérea permeável; monitorizar sinais vitais continuamente;\n"
-                 "- Preparar documentação médica e histórico; organizar transporte assistido (ambulância) se possível;\n"
-                 "- Contatos: acionar serviço de emergência local;\n"
-                 "- Observações adicionais: considerar suporte ventilatório e disponibilidade de leitos de UTI.\n");
+               "Paciente em estado CRÍTICO. AÇÕES IMEDIATAS:\n"
+               "- Encaminhar para emergência/UTI imediatamente; não administrar medicamentos sem prescrição;\n"
+               "- Evitar esforços, manter via aérea permeável; monitorizar sinais vitais continuamente;\n"
+               "- Preparar documentação médica e histórico; organizar transporte assistido (ambulância) se possível;\n"
+               "- Contatos: acionar serviço de emergência local;\n"
+               "- Observações adicionais: considerar suporte ventilatório e disponibilidade de leitos de UTI.\n");
     }
-    else if (strcmp(conclusao, "GRAVE")==0)
+    else if (strcmp(conclusao, " GRAVE	") == 0)
     {
         strcpy(orientacoes,
-                 "Paciente em estado GRAVE. AÇÕES RECOMENDADAS:\n"
-                 "- Avaliação presencial com prioridade nas próximas horas; realizar exames (oximetria, RX toráxico, hemograma).\n"
-                 "- Monitorar temperatura, frequência respiratória e oximetria a cada 2?4 horas; procurar urgência se houver queda de SpO2 < 94%% ou piora respiratória;\n"
-                 "- Isolamento e uso de máscara, hidratação e evitar anti-inflamatórios sem orientação;\n"
-                 "- Orientar transporte seguro e acompanhamento por profissional de saúde.\n");
+               "Paciente em estado GRAVE. AÇÕES RECOMENDADAS:\n"
+               "- Avaliação presencial com prioridade nas próximas horas; realizar exames (oximetria, RX toráxico, hemograma).\n"
+               "- Monitorar temperatura, frequência respiratória e oximetria a cada 2?4 horas; procurar urgência se houver queda de SpO2 < 94%% ou piora respiratória;\n"
+               "- Isolamento e uso de máscara, hidratação e evitar anti-inflamatórios sem orientação;\n"
+               "- Orientar transporte seguro e acompanhamento por profissional de saúde.\n");
     }
-    else if (strcmp(conclusao, "MODERADO")==0)
+    else if (strcmp(conclusao, " MODERADO	") == 0)
     {
         strcpy(orientacoes,
-                 "Quadro MODERADO. Tratamento e monitoramento:\n"
-                 "- Isolamento domiciliar e observação: medir temperatura 2x/dia e oximetria quando disponível;\n"
-                 "- Sintomas a monitorar: febre persistente (>38,5°C >48h), dispneia, confusão, sonolência excessiva ? procurar atendimento se ocorrerem;\n"
-                 "- Manter hidratação, repouso, antipiréticos se necessário (paracetamol) e evitar automedicação;\n"
-                 "- Reavaliar em 48?72 horas ou antes se houver piora.\n");// snprintf() : serve para formatar
+               "Quadro MODERADO. Tratamento e monitoramento:\n"
+               "- Isolamento domiciliar e observação: medir temperatura 2x/dia e oximetria quando disponível;\n"
+               "- Sintomas a monitorar: febre persistente (>38,5°C >48h), dispneia, confusão, sonolência excessiva ? procurar atendimento se ocorrerem;\n"
+               "- Manter hidratação, repouso, antipiréticos se necessário (paracetamol) e evitar automedicação;\n"
+               "- Reavaliar em 48?72 horas ou antes se houver piora.\n"); // snprintf() : serve para formatar
     }
-    else if (strcmp(conclusao, "LEVE")==0 || strcmp(conclusao, "BOM")==0)
+    else if (strcmp(conclusao, " LEVE	") == 0 )
     {
         strcpy(orientacoes,
-                 "Quadro leve ou sem indicativos graves. Cuidados domiciliares:\n"
-                 "- Hidratar, repousar, usar antipirético se necessário; manter isolamento até melhora de 24?48h dos sintomas;\n"
-                 "- Orientações de retorno ao trabalho/escola: aguardar 24?48h sem febre e melhora sustentada; vacinação e prevenção respiratória recomendadas;\n"
-                 "- Procurar assistência se surgirem sinais de alarme (dispneia, confusão, queda acentuada da saturação).\n");
+               "Quadro leve ou sem indicativos graves. Cuidados domiciliares:\n"
+               "- Hidratar, repousar, usar antipirético se necessário; manter isolamento até melhora de 24?48h dos sintomas;\n"
+               "- Orientações de retorno ao trabalho/escola: aguardar 24?48h sem febre e melhora sustentada; vacinação e prevenção respiratória recomendadas;\n"
+               "- Procurar assistência se surgirem sinais de alarme (dispneia, confusão, queda acentuada da saturação).\n");
     }
     else
     {
@@ -294,8 +297,8 @@ void observacoes()
     printf("\n");
     printf("\n		Paciente: %s\n", nome);
     printf("\n		Idade: %d\n", idade);
-    printf("\n		Contato: %s\n",telefone);
-    printf("\n		Pontos de Urgência: %d\n\n",pontos);
+    printf("\n		Contato: %s\n", telefone);
+    printf("\n		Pontos de Urgência: %d\n\n", pontos);
     printf("\n		%s\n", orientacoes);
     printf("\n");
     printf("========================================================================================================================\n\n");
@@ -304,9 +307,14 @@ void observacoes()
 // ----- URGÊNCIA POR PONTUAÇÃO -----
 void calculoUrgencia(int p)
 {
-    if (p >= 90) strcpy(urgencia, "CRITICA");
-    else if (p >= 70) strcpy(urgencia, "ALTA");
-    else if (p >= 45) strcpy(urgencia, "MEDIA");
-    else if (p >= 20) strcpy(urgencia, "BAIXA");
-    else strcpy(urgencia, "NULA");
+    if (p >= 90)
+        strcpy(urgencia, "CRITICA");
+    else if (p >= 70)
+        strcpy(urgencia, "ALTA");
+    else if (p >= 45)
+        strcpy(urgencia, "MEDIA");
+    else if (p >= 20)
+        strcpy(urgencia, "BAIXA");
+    else
+        strcpy(urgencia, "NULA");
 }
